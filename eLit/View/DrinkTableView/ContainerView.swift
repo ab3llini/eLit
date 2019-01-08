432a9f1b041ff85878b9845a5d9f3e2190113e50
ab3llini <albertomario.bellini@mail.polimi.it>
2019-01-08 11:58:36 +0100

    Added container view in drink table view

---
+++ eLit/View/DrinkTableView/ContainerView.swift
@@ -0,0 +1,21 @@
+//
+//  ColorMask.swift
+//  eLit
+//
+//  Created by Alberto Mario Bellini on 06/01/2019.
+//  Copyright © 2019 Alberto Mario Bellini. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class ContainerView: UIView {
+    
+    @IBInspectable public var borderRadius : CGFloat = 0
+
+    override func awakeFromNib() {
+        self.layer.cornerRadius = borderRadius
+    }
+    
+
+}
+
