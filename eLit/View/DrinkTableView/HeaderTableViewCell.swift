2385ec9fda6eb90d10b3bd6334d5e07cff368b78
ab3llini <albertomario.bellini@mail.polimi.it>
2019-01-10 18:05:31 +0100

    Added carousel

--- eLit/View/DrinkTableView/HeaderTableViewCell.swift
+++ eLit/View/DrinkTableView/HeaderTableViewCell.swift
@@ -8,11 +8,40 @@
 
 import UIKit
 
-class HeaderTableViewCell: UITableViewCell {
+class HeaderTableViewCell: UITableViewCell, FSPagerViewDelegate, FSPagerViewDataSource {
+    
+    
+    let drinks = Model.getInstance().getDrinks()
+    
+    @IBOutlet weak var drinkPagerView: FSPagerView! {
+        didSet {
+            self.drinkPagerView.register(FSPagerViewCell.self, forCellWithReuseIdentifier: "cell")
+        }
+    }
+    
+    func numberOfItems(in pagerView: FSPagerView) -> Int {
+        return drinks.count
+    }
+    
+    func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
+        let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "cell", at: index)
+        cell.imageView?.image = UIImage(named: drinks[index].image!)
+        cell.textLabel?.text = drinks[index].description
+        return cell
+    }
+    
 
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        drinkPagerView.delegate = self
+        drinkPagerView.dataSource = self
+        
+        self.drinkPagerView.transformer = FSPagerViewTransformer(type: .cubic)
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
