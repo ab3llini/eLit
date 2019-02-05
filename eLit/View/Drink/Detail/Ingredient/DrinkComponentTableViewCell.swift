62a5ac37de99cad9674b4182b3289ec41a01ddf5
gianpaolo <gianpaolo.dipietro@mail.polimi.it>
2019-02-05 12:24:34 +0100

    Add nib for ingredients

---
+++ eLit/View/Drink/Detail/Ingredient/DrinkComponentTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  DrinkComponentTableViewCell.swift
+//  eLit
+//
+//  Created by Gianpaolo Di Pietro on 05/02/2019.
+//  Copyright © 2019 eLit.app. All rights reserved.
+//
+
+import UIKit
+
+class DrinkComponentTableViewCell: UITableViewCell {
+    @IBOutlet weak var ingredientImageView: UIImageView!
+    @IBOutlet weak var qtyLabel: UILabel!
+    @IBOutlet weak var unitLabel: UILabel!
+    @IBOutlet weak var ingredientLabel: UILabel!
+    @IBOutlet weak var componentView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        componentView.layer.cornerRadius = componentView.frame.height / 2
+        
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func rounded(radius: CGFloat?, withBorder border: CGFloat, withBorderColor borderColor: UIColor) {
+        self.componentView.layer.borderWidth = border
+        self.componentView.layer.borderColor = borderColor.cgColor
+        guard let r = radius else {
+            self.componentView.layer.cornerRadius = self.componentView.frame.height / 2
+            return
+        }
+        
+        self.componentView.layer.cornerRadius = r
+    }
+    
+}
