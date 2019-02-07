3c3e31c584d41549e107f174e079cca701949340
ab3llini <albertomario.bellini@mail.polimi.it>
2019-02-07 18:31:20 +0100

    Working on backend

--- eLitBackend/backend.js
+++ eLitBackend/backend.js
@@ -1,5 +1,14 @@
+// ------------------------------------------- CONFIG -------------------------------------------
+
 let server = '127.0.0.1'
 let port = '9999'
+
+// ------------------------------------------- GLOBALS -------------------------------------------
+
+let ingredient_names = []
+let units = ['part']
+
+// ------------------------------------------- CLASSES -------------------------------------------
 
 
 class Connection {
@@ -7,17 +16,17 @@ class Connection {
     constructor(server, port) {
 
 
-        this.ws = new WebSocket('ws://'+server+':'+port+'/')
+        this.ws = new WebSocket('ws://' + server + ':' + port + '/')
 
         this.handlers = {}
 
         let _self = this
 
-        this.ws.onopen = function() {
+        this.ws.onopen = function () {
             console.log("Socket opened")
         }
 
-        this.ws.onerror = function() {
+        this.ws.onerror = function () {
             console.log("Socket error")
         }
         this.ws.onmessage = function (event) {
@@ -41,8 +50,7 @@ class Connection {
 
             console.log('No handler for request: ' + payload.request)
 
-        }
-        else {
+        } else {
 
             handler(payload.data)
 
@@ -54,8 +62,6 @@ class Connection {
 
 let connection = new Connection(server, port)
 
-// ------------------------------------------- UTILS ----------------------------------------------
-
 
 // ------------------------------------------- HANDLERS -------------------------------------------
 
@@ -63,22 +69,29 @@ let ws_ingredient_handler = (ingredients) => {
 
     ingredients.reverse().forEach(function (ingredient) {
 
-        $(".ingredient").DataTable().row.add( [
+        // Add ingredient name to global var
+        ingredient_names.push(ingredient.name)
+
+        $(".ingredient").DataTable().row.add([
             ingredient.name,
             ingredient.grade,
             ingredient.image,
             ingredient.ingredient_description
-        ] ).draw( false );
+        ]).draw(false);
 
     })
 
 }
 
-let table_new_line_handler = (_class, _conn) => {
+
+// ------------------------------------------- UTILS ----------------------------------------------
+
+
+let on_add_ingredient = (_class, _conn) => {
 
     let table = $(".new-" + _class).last()
 
-    let request = {request: 'insert_' + _class , data : {}}
+    let request = {request: 'insert_' + _class, data: {}}
 
     table.find('input').each(function () {
 
@@ -97,7 +110,50 @@ let table_new_line_handler = (_class, _conn) => {
     _conn.ws.send(JSON.stringify(request))
 
     ws_ingredient_handler([request.data])
+
+    update_ingredient_dropdowns()
+
 }
+
+let on_add_drink = () => {
+
+    let form = $('.new-drink-form')
+
+    let request = {request: 'insert_drink', data: {}}
+
+    data.name =
+        
+
+}
+
+let create_options_for = (list) => {
+
+    let options = ''
+
+    list.forEach(function (o) {
+
+        options += '<option>' + o + '</option>'
+
+    })
+
+    return options
+
+}
+
+let update_ingredient_dropdowns = () => {
+
+    $('.components').find('.ingredient-select').append(create_options_for(ingredient_names))
+
+}
+
+let update_unit_dropdowns = () => {
+
+    $('.components').find('.unit-select').append(create_options_for(units))
+
+}
+
+
+
 
 
 // ------------------------------------------- BINDING ---------------------------------------------
@@ -109,19 +165,85 @@ connection.bind('fetch_ingredients', ws_ingredient_handler)
 
 $(document).ready(function () {
 
-    let tables = ['ingredient', 'drink']
 
-    tables.forEach(function (table) {
+    let new_drink_table = $('.new-ingredient')
 
-        let t = $('.new-' + table)
+    new_drink_table.on("click", ".add-ingredient", function () {
 
-        t.on("click", ".add-" + table, function () {
+        on_add_ingredient('ingredient', connection)
 
-            table_new_line_handler(table, connection)
+    })
 
+    // Handle new recipe step add
+    $('.recipe').on('click', '.add-recipe-step', function () {
+
+        $(this).closest('.recipe').append(
+            '<div class="form-row">\n' +
+            '<div class="form-group col-md-12">\n' +
+            '<label>Recipe step</label>\n' +
+            '<div class="input-group">\n' +
+            '<input type="text" class="form-control" placeholder="Description" name="description">\n' +
+            '<div class="input-group-append">\n' +
+            '<button class="btn btn-outline-danger remove-recipe-step" type="button">' +
+            '<i class="fa fa-minus"></i>\n' +
+            '</button>\n' +
+            '<button class="btn btn-outline-primary add-recipe-step" type="button">' +
+            '<i class="fa fa-plus"></i>\n' +
+            '</button>\n' +
+            '</div>\n' +
+            '</div>\n' +
+            '</div>\n' +
+            '</div>'
+        )
+
+        $('.recipe').on('click', '.remove-recipe-step', function () {
+            $(this).closest('.form-row').remove()
+        })
+
+
+    })
+
+    // Handle new recipe step add
+    $('.components').on('click', '.add-component', function () {
+
+        $(this).closest('.components').append(
+            '<div class="form-row">\n' +
+            '<div class="form-group col-md-12">\n' +
+            '<label>Component</label>\n' +
+            '<div class="input-group">\n' +
+            '<input type="number" class="form-control" placeholder="Quantity" name="quantity">\n' +
+            '<select class="custom-select unit-select" name="unit">\n' +
+            '<option selected>Choose...</option>\n' +
+            create_options_for(units) +
+            '</select>\n' +
+            '<select class="custom-select ingredient-select" name="ingredient">\n' +
+            '<option selected>Choose...</option>\n' +
+            create_options_for(ingredient_names) +
+            '</select>\n' +
+            '<div class="input-group-append">\n' +
+            '<button class="btn btn-outline-danger remove-component" type="button">\n' +
+            '<i class="fa fa-minus"></i>\n' +
+            '</button>\n' +
+            '<button class="btn btn-outline-primary add-component" type="button">\n' +
+            '<i class="fa fa-plus"></i>\n' +
+            '</button>\n' +
+            '</div>\n' +
+            '</div>\n' +
+            '</div>\n' +
+            '</div>'
+        )
+
+        $('.components').on('click', '.remove-component', function () {
+            $(this).closest('.form-row').remove()
         })
 
     })
 
 
+    // Update dropdowns on load
+
+    update_ingredient_dropdowns()
+    update_unit_dropdowns()
+
 })
+
