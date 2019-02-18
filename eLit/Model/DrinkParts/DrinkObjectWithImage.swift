ef061265ee26bdd737eefcb703d2daae1a9a0d6d
gianpaolo <gianpaolo.dipietro@mail.polimi.it>
2019-02-18 11:32:43 +0100

    Image downloading refactor

--- eLit/Model/DrinkParts/DrinkObjectWithImage.swift
+++ eLit/Model/DrinkParts/DrinkObjectWithImage.swift
@@ -11,16 +11,29 @@ import UIKit
 @objc(DrinkObjectWithImage)
 class DrinkObjectWithImage: DrinkObject {
     
-    var image: UIImage = UIImage()
+    var image: UIImage?
     
-    func setImage(forceReload: Bool = false) {
+    func setImage(forceReload: Bool = false, completion: ((_ image: UIImage?) -> Void)? = nil) {
         if forceReload {
-             getImageData(forceReload: true)
+            getImageData(forceReload: true, completion: completion)
         }
+        // The image is already initialized
+        if let img = self.image, let actualCompletion = completion {
+            DispatchQueue.main.async {
+                actualCompletion(img)
+            }
+            return
+        }
+        // The image is not initialized but is present in the database
         if let id = self.imageData {
             self.image = UIImage(data: id) ?? UIImage()
+            if let actualCompletion = completion {
+                DispatchQueue.main.async {
+                    actualCompletion(self.image)
+                }
+            }
         } else {
-            getImageData(forceReload: true)
+            getImageData(forceReload: true, completion: completion)
             guard let _ = self.imageData else {
                 self.image = UIImage()
                 return
@@ -29,13 +42,8 @@ class DrinkObjectWithImage: DrinkObject {
         
     }
     
-    func setImage(forceReload: Bool = false, completion: (_ image: UIImage?) -> Void) {
-        self.setImage(forceReload: forceReload)
-        completion(self.image)
-    }
     
-    
-    private func getImageData(forceReload: Bool) {
+    private func getImageData(forceReload: Bool, completion: ((_ image: UIImage?) -> Void)?) {
         if self.imageData != nil && (!forceReload) {
             return
         }
@@ -46,16 +54,36 @@ class DrinkObjectWithImage: DrinkObject {
         
         print("Requesting asset: \(url)")
         
-        if let id: NSData = NSData(contentsOf: url) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, let actualCompletion = completion, error == nil else {                                                 // check for fundamental networking error
+                NSLog("error=\(String(describing: error))")
+                if let actualCompletion = completion {
+                    DispatchQueue.main.async {
+                        actualCompletion(UIImage())
+                    }
+                }
+                return
+            }
+        
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                DispatchQueue.main.async {
+                    actualCompletion(UIImage())
+                }
+                return
+            }
             
-            self.imageData = id as Data
-            self.image = UIImage(data: self.imageData!) ?? UIImage()
-        } else { return }
+            DispatchQueue.main.async {
+                self.imageData = data
+                self.image = UIImage(data: data)
+            }
+        }
+        
+        task.resume()
     }
     
     override func update(with data: [String : Any], savePersistent: Bool) {
-        self.getImageData(forceReload: true)
-        super.update(with: data, savePersistent: true)
+        self.getImageData(forceReload: true, completion: { img in super.update(with: data, savePersistent: savePersistent)})
     }
 
 }
