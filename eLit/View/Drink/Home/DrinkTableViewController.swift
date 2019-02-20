e29bf637280e4635712b34c18c90cf3ed49c2e22
ab3llini <albertomario.bellini@mail.polimi.it>
2019-02-20 11:18:26 +0100

    Foxed bug

--- eLit/View/Drink/Home/DrinkTableViewController.swift
+++ eLit/View/Drink/Home/DrinkTableViewController.swift
@@ -170,7 +170,7 @@ class DrinkTableViewController: BlurredBackgroundTableViewController, UINavigati
             
             UIView.animate(withDuration: 0.1, delay: 0, options: [.curveEaseInOut], animations: {
                 
-                selected.backgroundImage.backgroundColor = selected.backgroundImage.backgroundColor!.withAlphaComponent(0.4)
+                selected.containerView.backgroundColor = selected.containerView.backgroundColor!.withAlphaComponent(0.4)
                 
                 self.needBgReset = selected
                 
@@ -227,7 +227,7 @@ class DrinkTableViewController: BlurredBackgroundTableViewController, UINavigati
         
         if let cell = needBgReset {
             
-            cell.backgroundImage.backgroundColor = cell.backgroundImage.backgroundColor!.withAlphaComponent(0.1)
+            cell.containerView.backgroundColor = cell.containerView.backgroundColor!.withAlphaComponent(0.1)
 
             needBgReset = nil
             
@@ -278,44 +278,6 @@ class DrinkTableViewController: BlurredBackgroundTableViewController, UINavigati
         }
         
      }
-    
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
 
 
 }
