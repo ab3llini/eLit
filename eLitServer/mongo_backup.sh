fb17a6cedecec9341081dd05ff60409d8c59a516
gianpaolo <gianpaolo.dipietro@mail.polimi.it>
2019-02-22 12:56:13 +0100

    add backup script mongodb

---
+++ eLitServer/mongo_backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ 
+MONGO_DATABASE="eLit"
+APP_NAME="eLit"
+
+MONGO_HOST="127.0.0.1"
+MONGO_PORT="27017"
+TIMESTAMP=`date +%F-%H%M`
+MONGODUMP_PATH="/usr/bin/mongodump"
+BACKUPS_DIR="/root/backups/$APP_NAME"
+BACKUP_NAME="$APP_NAME-$TIMESTAMP"
+ 
+# mongo admin --eval "printjson(db.fsyncLock())"
+# $MONGODUMP_PATH -h $MONGO_HOST:$MONGO_PORT -d $MONGO_DATABASE
+$MONGODUMP_PATH -d $MONGO_DATABASE
+# mongo admin --eval "printjson(db.fsyncUnlock())"
+ 
+mkdir -p $BACKUPS_DIR
+mv dump $BACKUP_NAME
+zip -r $BACKUPS_DIR/$BACKUP_NAME.zip $BACKUP_NAME
+rm -rf $BACKUP_NAME
