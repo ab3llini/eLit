cae7cbe3f09477349d5c41f5b6335a0400bc2a7a
gianpy15 <gpdipietro@gmail.com>
2019-04-08 16:38:44 +0200

    Fix data type

--- eLit/Network/ResourceManager.swift
+++ eLit/Network/ResourceManager.swift
@@ -12,9 +12,26 @@ typealias CompletionHandler<T>  = (_ result: T) -> Void
 
 class ResourceManager: NSObject {
     
+    // MARK: attributes
     public static let shared = ResourceManager()
+    private var completion_map = Dictionary<String, [CompletionHandler<Data?>]>()
+    private var dataMap = Dictionary<String, Data?>()
     
-    public func fetchImageData(from relativeURL : String, onCompletion handler : CompletionHandler<NSData>) {
+    
+    public func fetchImageData(from relativeURL : String, onCompletion handler : @escaping CompletionHandler<Data?>) {
+        if let completions = self.completion_map[relativeURL] {
+            // The image has already been requested
+            
+            if completions.isEmpty {
+                // I already have the data, just call the completion
+                DispatchQueue.main.async {
+                    handler(self.dataMap[relativeURL]!)
+                }
+            }
+            completion_map[relativeURL]?.append(handler)
+        } else {
+            // The image has to be requested
+        }
     
         //URL(string: Preferences.shared.coreSettings.host + self.imageURLString!) else {
 
