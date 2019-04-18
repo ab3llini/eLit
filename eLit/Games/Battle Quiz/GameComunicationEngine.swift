cb3c3392dc19c30279e1ddf705e0c7907b37b28a
gianpy15 <gpdipietro@gmail.com>
2019-04-18 18:07:22 +0200

    Working on game engine

---
+++ eLit/Games/Battle Quiz/GameComunicationEngine.swift
@@ -0,0 +1,40 @@
+//
+//  GameComunicationEngine.swift
+//  eLit
+//
+//  Created by Gianpaolo Di Pietro on 18/04/2019.
+//  Copyright © 2019 eLit.app. All rights reserved.
+//
+
+import UIKit
+
+class GameComunicationEngine: NSObject {
+    
+    
+    private let mode: OperationMode
+    private let questionGenerator: QuestionGenerator?
+    private let cm = ConnectionManager.shared
+    
+    init (for mode: OperationMode) {
+        self.mode = mode
+        if mode == .host {
+            self.questionGenerator = QuestionGenerator()
+        } else {
+            self.questionGenerator = nil
+        }
+        
+        super.init()
+    }
+    
+    func getNextQuestion(then completion: (_ question: Question?) -> Void) {
+        switch self.mode {
+        case .host:
+            let question = self.questionGenerator?.getQuestion()
+            completion(question)
+        case .client:
+            self.cm.requestQuestion(then: completion)
+        }
+    }
+    
+
+}
